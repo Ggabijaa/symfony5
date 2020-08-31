ae82681ec5d85348bbdcc52c2ad6c783e28e3e91
Gabija <g.bruzgaite@gmail.com>
2020-08-31 16:28:17 +0300

    add conference page view, add all conferences view

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,22 +2,49 @@
 
 namespace App\Controller;
 
-use http\Env\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Component\HttpFoundation\Request;
+use \Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
 
 class ConferenceController extends AbstractController
 {
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
-    public function index()
+    public function index(ConferenceRepository $conferenceRepository)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ConferenceController.php',
-        ]);
+        return new Response($this->twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
 
+    }
+
+    /**
+     * @Route("/conference/{id}", name="conference")
+     */
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository)
+    {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        return new Response($this->twig->render('conference/show.html.twig', [
+            'conference'=>$conference,
+            'comments' =>$paginator,
+            'previous'=>$offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator),$offset + CommentRepository::PAGINATOR_PER_PAGE),
+        ]));
     }
 }
